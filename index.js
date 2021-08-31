bc46e89a5e4f617e7079cf0e961c804da7666138
Karim Badwork <karim@badwork.fr>
2021-09-01 01:26:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const e = React.createElement;
+
+class Greeting extends React.Component {
+    render() {
+        return (<p>Hello world</p>);
+    }
+}
+ReactDOM.render(
+    <Greeting />,
+    document.getElementById('root')
+);
